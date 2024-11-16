6a42f128b34ae46b1d57438ee2da72700ea23efe
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-16 20:54:56 +0700

    feat: add animation on Rifki

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 // Array berisi nama font yang diinginkan
 const fonts = ['Arial', 'Georgia', 'Courier New', 'Times New Roman', 'Verdana', 'Tahoma', 'Lucida Console'];
 
+let intervalId;
+
 function changeFontRandomly() {
     const element = document.getElementById('dynamic-font');
     const randomFont = fonts[Math.floor(Math.random() * fonts.length)];
     element.style.fontFamily = randomFont;
 }
 
-// Ubah font setiap 500 ms
-const intervalId = setInterval(changeFontRandomly, 500);
+// Fungsi untuk memulai animasi
+function startAnimation() {
+    intervalId = setInterval(changeFontRandomly, 300);
+}
 
-// Hentikan animasi saat elemen di-hover
-document.getElementById('dynamic-font').addEventListener('mouseover', function() {
+// Fungsi untuk menghentikan animasi
+function stopAnimation() {
     clearInterval(intervalId);
+}
+
+// Mulai animasi saat halaman dimuat
+startAnimation();
+
+// Hentikan animasi saat di-hover
+document.getElementById('dynamic-font').addEventListener('mouseover', function() {
+    stopAnimation();
+});
+
+// Lanjutkan animasi saat hover selesai
+document.getElementById('dynamic-font').addEventListener('mouseout', function() {
+    startAnimation();
 });
